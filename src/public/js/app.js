ae4c6cac9a73f9893b344c60f4ff93bd1185b158
Jung-Ah-C <wjddk718@gmail.com>
2022-08-30 14:57:22 +0900

    WebSocket 프론트엔드 백엔드 양방향 연결 설정

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,19 @@
-alert("hi")
+const socket = new WebSocket(`ws://${window.location.host}`); // 여기서 socket은 서버로의 연결을 뜻함
+
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server ✅");
+});
+
+socket.addEventListener("message", (message) => {
+    console.log("New message: ", message.data);
+});
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server ❌");
+});
+
+
+setTimeout(() => {
+    socket.send("hello from the browser!");
+}, 10000)
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -6,8 +8,24 @@ const app = express();
 app.set("view engine", "pug"); // Pug로 view engine 설정
 app.set("views", __dirname + "/views"); // Express에 template이 어딨는지 지정
 app.use("/public", express.static(__dirname + "/public")); // public url을 생성해서 유저에게 파일을 공유
-app.get("/", (req, res) => res.render("home")); // home.pug를 render 해주는 route handler를 만들어줌
-app.get("*/", (req, res) => res.redirect("/")); // redirect url 설정
+app.get("/", (_, res) => res.render("home")); // home.pug를 render 해주는 route handler를 만들어줌
+app.get("*/", (_, res) => res.redirect("/")); // redirect url 설정
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen); // port 3000으로 설정
+
+// 같은 포트에 서버와 웹소켓을 같이 실행시키기 위한 설정
+// http 서버에 접근해서 그 서버위에 webSocket 서버를 만들 수 있게 함
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server }); // websocket 서버 생성
+
+wss.on("connection", (socket) => { // 여기서 socket은 연결된 브라우저를 뜻함
+    console.log("Connected to Browser ✅");
+    // 새로운 브라우저가 접속하게 되면 socket.on 코드가 실행됨
+    socket.on("close", () => console.log("Disconnected from the Browser ❌")) // frontend에서의 close 이벤트를 listen 하는 것 = 브라우저 탭을 닫으면 (브라우저 연결이 끊기면) 실행
+    socket.on("message", (message) => { // 브라우저가 서버에 메세지를 보냈을 때를 위한 listener
+        console.log(message.toString());
+    });
+    socket.send("hello :)"); // 브라우저에 메세지를 보냄
+});
+
+server.listen(3000, handleListen);
