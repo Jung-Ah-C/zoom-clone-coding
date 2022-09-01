509c375f6b1bf856871655be1a58cd132d271bb0
Jung-Ah-C <wjddk718@gmail.com>
2022-09-01 17:35:28 +0900

    채팅방 목록 기능 추가

--- src/server.js
+++ src/server.js
@@ -4,7 +4,6 @@ import express from "express";
 
 const app = express();
 
-
 app.set("view engine", "pug"); // Pug로 view engine 설정
 app.set("views", __dirname + "/views"); // Express에 template이 어딨는지 지정
 app.use("/public", express.static(__dirname + "/public")); // public url을 생성해서 유저에게 파일을 공유
@@ -18,26 +17,48 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
-wsServer.on("connection", socket => {
-    socket.onAny((event) => {
-        console.log(`Socket Event : ${event}`);
-    });
-    socket.on("enter_room", (roomName, done) => {
-        socket.join(roomName); // socket.io에 있는 room 기능
-        done();
-        socket.to(roomName).emit("welcome");
-    });
-    socket.on("disconnecting", () => {
-        socket.rooms.forEach(room => {
-            socket.to(room).emit("bye");
-        });
-    });
-    socket.on("new_message", (msg, room, done) => {
-        socket.to(room).emit("new_message", msg);
-        done();
-    });
-});
+function publicRooms() {
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    },
+  } = wsServer;
+  const publicRooms = [];
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
+    }
+  });
+  return publicRooms;
+}
 
+wsServer.on("connection", (socket) => {
+  socket["nickname"] = "Anon";
+  socket.onAny((event) => {
+    console.log(`Socket Event : ${event}`);
+  });
+  socket.on("enter_room", (roomName, done) => {
+    socket.join(roomName); // socket.io에 있는 room 기능
+    done();
+    socket.to(roomName).emit("welcome", socket.nickname);
+    wsServer.sockets.emit("room_change", publicRooms());
+  });
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach((room) => {
+      socket.to(room).emit("bye", socket.nickname);
+    });
+  });
+  socket.on("disconnect", () => {
+    wsServer.sockets.emit("room_change", publicRooms());
+  });
+  socket.on("new_message", (msg, room, done) => {
+    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
+    done();
+  });
+  socket.on("nickname", (nickname) => {
+    socket["nickname"] = nickname;
+  });
+});
 
 /* 
 const wss = new WebSocket.Server({ server }); // websocket 서버 생성
