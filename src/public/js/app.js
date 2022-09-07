8a98054c2d8eb6baf413ac7038c243b3840aa5bc
Jung-Ah-C <wjddk718@gmail.com>
2022-09-07 14:13:07 +0900

    사용자가 카메라를 선택할 수 있도록 기능 구현

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -13,12 +13,15 @@ let cameraOff = false;
 async function getCameras() {
   try {
     const devices = await navigator.mediaDevices.enumerateDevices();
-    console.log(devices);
     const cameras = devices.filter(device => device.kind === "videoinput");
-    cameras.forEach(camera => {
-      const option = document.getElementById("option");
+    const currentCamera = myStream.getVideoTracks()[0];
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
       option.value = camera.deviceId;
       option.innerText = camera.label;
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+      }
       camerasSelect.appendChild(option);
     })
   } catch(e) {
@@ -26,13 +29,23 @@ async function getCameras() {
   }
 }
 
-async function getMedia() {
+async function getMedia(deviceId) {
+  const initialConstrains = {
+    audio: true,
+    video: { facingMode: "user" },
+  };
+  const cameraConstraints = {
+    audio: true,
+    video: { deviceId: { exact: deviceId } },
+  }
   try {
-    myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
-    });
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstraints : initialConstrains
+    );
     myFace.srcObject = myStream;
+    if(!deviceId) {
+      await getCameras();
+    }
   } catch(e) {
     console.log(e);
   }
@@ -62,5 +75,10 @@ function handleCameraClick() {
   }
 }
 
+async function handleCameraChange() {
+  await getMedia(camerasSelect.value);
+}
+
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
+camerasSelect.addEventListener("input", handleCameraChange);
